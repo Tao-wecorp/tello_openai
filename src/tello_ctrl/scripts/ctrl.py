f8380391535fddd8eb8d70c02f711e4071adf467
boringtao <xinghui.tao@wecorpindustries.com>
2020-05-14 08:40:04 +0100

    Add: vision opencv for python3

--- src/tello_ctrl/scripts/ctrl.py
+++ src/tello_ctrl/scripts/ctrl.py
@@ -22,11 +22,39 @@ class Tello(object):
         self._drone.wait_for_connection(60.0)
         rospy.loginfo('connected to drone')
 
-        self._drone.takeoff()
+        self._image_pub = rospy.Publisher('tello/image_raw', Image, queue_size=10)
+        self._cv_bridge = CvBridge()
+        # Start video thread
+        self._stop_request = threading.Event()
+        video_thread = threading.Thread(target=self.video_worker)
+        video_thread.start()
+
+        # self._drone.takeoff()
         rospy.on_shutdown(self.shutdown)
+
+        rospy.spin()
+    
+    def video_worker(self):
+        container = av.open(self._drone.get_video_stream())
+
+        rospy.loginfo('starting video pipeline')
+        
+        for frame in container.decode(video=0):
+            # PyAV frame => PIL image => OpenCV Mat
+            color = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+
+            print(color)
+
+            # color_mat = cv2.cvtColor(numpy.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+            # self._image_pub.publish(self._cv_bridge.cv2_to_imgmsg(color_mat, 'bgr8'))
+
+            if self._stop_request.isSet():
+                return
 
     def shutdown(self):
         self._drone.land()
+        self._stop_request.set()
+        video_thread.join(timeout=2)
         self._drone.quit()
         self._drone = None
 
@@ -35,6 +63,8 @@ def main():
         Tello()
     except KeyboardInterrupt:
         pass
+    finally:
+        cv2.destroyAllWindows()
     
 
 if __name__ == '__main__':
