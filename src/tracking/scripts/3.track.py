bf55fd66af3998a4c3640d8d0eed5df29e7fd2cf
boringtao <xinghui.tao@wecorpindustries.com>
2020-07-20 11:54:18 +0100

    Add: msg

--- src/tracking/scripts/3.track.py
+++ src/tracking/scripts/3.track.py
@@ -10,6 +10,7 @@ rospack = rospkg.RosPack()
 from std_msgs.msg import Float64, String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from tracking.msg import Target
 
 # Helpers
 from helpers.cvlib import Detection
@@ -27,6 +28,8 @@ class Detect(object):
 
         rospy.Subscriber('/tello/image_raw', Image, self.img_callback)
         rospy.Subscriber('/keypress', String, self.key_callback)
+
+        self.target_pub = rospy.Publisher('tello/target', BBox, queue_size=1)
         
         while not rospy.is_shutdown():
             if self.frame is not None:
@@ -35,8 +38,7 @@ class Detect(object):
                 if len(centroids) != 0:
                     # if self.keypress != -1 and self.keypress < len(centroids):
                     #     cent = centroids[self.keypress]
-                    cent = centroids[0]
-                    target = int(centroids[self.keypress][0]-480)
+                    target = centroids[0]
                     print(target)
                     
                     cv2.circle(frame, (cent[0], cent[1]), 3, [0,0,255], -1, cv2.LINE_AA)
